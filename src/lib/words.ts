cbb1204f89072a5a8cace3c8f2ae7016dfef61d7
sefaozturkmen <sefaozturkmenn@gmail.com>
2024-12-22 03:07:15 +0300

    update

---
+++ src/lib/words.ts
@@ -0,0 +1,8 @@
+export const commonWords = [
+  { en: "the", tr: "belirli artikel" },
+  { en: "be", tr: "olmak" },
+  { en: "to", tr: "-e/-a" },
+  { en: "of", tr: "-in/-ın" },
+  { en: "and", tr: "ve" },
+  // ... diğer kelimeler
+]; 
